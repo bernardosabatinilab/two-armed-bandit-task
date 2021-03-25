59d6bb0d6de25cd3b34e53c68975cc75f0625d1e
jbwallace123 <41006280+jbwallace123@users.noreply.github.com>
2021-03-25 14:27:00 -0400

    Delete Mouse-Bandit-Behavior-markov/Mouse-Bandit-Behavior-laser directory

--- Mouse-Bandit-Behavior-markov/Mouse-Bandit-Behavior-laser/triplePortCode_v5/logEvent.m
+++
@@ -1,3 +0,0 @@
-function logEvent(eventName)
-    logValue(eventName, '')
-end
--- Mouse-Bandit-Behavior-markov/Mouse-Bandit-Behavior-laser/triplePortCode_v5/updatestatsfig.m
+++
@@ -1,8 +0,0 @@
-function updatestatsfig(stats,h,pokeCount)
-%updatestatsfig(stats,h)
-XPLOT = 1:pokeCount;
-
-for i = 1:size(h,2)
-    refreshdata(h(i),'caller')
-end
-
